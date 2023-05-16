7ee968155592eb6294d9f7dbc38cb6f35d823167
okan kaipoglu <okan_kaipoglu@outlook.com>
2023-05-17 01:54:23 +0300

    tatil yerleri listelendi

--- HolidayCoreProject/ViewComponents/Default/_PopularDestinations.cs
+++ HolidayCoreProject/ViewComponents/Default/_PopularDestinations.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HolidayCoreProject.ViewComponents.Default
 {
     public class _PopularDestinations:ViewComponent
     {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = destinationManager.TGetList();
+             
+            return View(values);
         }
     }
 }
