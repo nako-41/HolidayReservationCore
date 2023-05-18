538d2639369cbedc2632c38f4ba14f4706d1f8a2
okan kaipoglu <okan_kaipoglu@outlook.com>
2023-05-18 13:45:29 +0300

    subabout ve testmonial tarafı duzenlendi dinamik hale getirildi

---
+++ HolidayCoreProject/ViewComponents/Default/_SubAbout.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HolidayCoreProject.ViewComponents.Default
+{
+    public class _SubAbout:ViewComponent
+    {
+        SubAboutManager SubAboutManager = new SubAboutManager(new EfSubAboutDal());
+        public IViewComponentResult Invoke()
+        {
+            var result = SubAboutManager.TGetList();
+
+            return View(result);
+        }
+
+
+    }
+}
---
+++ HolidayCoreProject/ViewComponents/Default/_Testimonial.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HolidayCoreProject.ViewComponents.Default
+{
+    public class _Testimonial:ViewComponent
+    {
+        TestimonialManager testimonialManager = new TestimonialManager(new EfTestimonialDal());
+        public IViewComponentResult Invoke()
+        {
+            var result = testimonialManager.TGetList();
+
+            return View(result);
+        }
+
+    }
+}
