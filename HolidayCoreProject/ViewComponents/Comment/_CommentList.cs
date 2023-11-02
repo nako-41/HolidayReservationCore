446f47f266816ea6bdf3c0d1969fefd14cd8b1f7
okan kaipoglu <okan_kaipoglu@outlook.com>
2023-11-03 01:47:07 +0300

    yine standartı bozmadan yorumları da componentlere parcaladım,component halinde duzenli gozukuyor

---
+++ HolidayCoreProject/ViewComponents/Comment/_CommentList.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HolidayCoreProject.ViewComponents.Comment
+{
+    public class _CommentList:ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
